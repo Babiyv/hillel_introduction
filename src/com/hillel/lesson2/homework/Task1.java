c81da7ffd852b5b3f3f8f27354d5bdee5eaee730
Babiyv <babiy.vladislav@gmail.com>
2021-03-17 13:01:12 +0200

    lesson2 -> homework -> Task2 done

---
+++ src/com/hillel/lesson2/homework/Task1.java
@@ -0,0 +1,29 @@
+package com.hillel.lesson2.homework;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Task1 {
+    public static void main(String[] args) {
+    /*1)   Создать переменную = 1 и используя инкремент и декремент напечатать (System.out.println)
+           1
+           0
+           1
+           1
+           1
+           2
+           1
+           1*/
+        int i = 1;
+        System.out.println(i);
+        System.out.println(--i);
+        System.out.println(++i);
+        System.out.println(i);
+        System.out.println(i++);
+        System.out.println(i--);
+        System.out.println(i);
+        System.out.println(i);
+
+
+
+    }
+}
