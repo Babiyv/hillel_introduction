e6fbbeb6cee5b594b1bc48ad3d26b7645e1b879d
Babiyv <babiy.vladislav@gmail.com>
2021-03-19 12:27:44 +0200

    Task5 is done (simplest way)

--- src/com/hillel/lesson4/homework/Task5.java
+++ src/com/hillel/lesson4/homework/Task5.java
@@ -6,9 +6,9 @@ public class Task5 {
     public static void main(String[] args) {
         int[] intArray = {2, 3, 1, 5, 8, 6, 7, 10, 9, 4};
         int max = intArray[0]; // - на тот случай, если наибольшее число у нас окажется в первой ячейке
-        for (int i = 0; i < intArray.length - 1; i++) {
-            if (max < intArray[i + 1]){
-                max = intArray[i + 1];
+        for (int i = 1; i < intArray.length; i++) {
+            if (max < intArray[i]){
+                max = intArray[i];
             }
         }
         System.out.println(max);
