95db2f4a57c7a30f463ee7c51e6dcfe7cb5a5a14
Babiyv <babiy.vladislav@gmail.com>
2021-04-11 14:10:23 +0300

    new classwork lesson8: Incapsulation

---
+++ src/com/hillel/lesson8/ClassWork8.java
@@ -0,0 +1,41 @@
+package com.hillel.lesson8;
+
+public class ClassWork8 {
+
+    static final String GIRL_SEX = "female"; // - константа позволяет скоратить время выполнения программы; константы объявляются(по конвенции) только в аппер-кейсе.
+
+    public static void main(String[] args) {
+
+//        GIRL_SEX = "male"; // - не даст присвоить male, потому что выше мы уже объявили,что это final и присвоили female; (константнта - не изменяемое значение)
+
+        IncapsulationExample incapsulationExample;
+        incapsulationExample = new IncapsulationExample(); // - можно было сразу создать в строке выше;
+        incapsulationExample.setName("MyName");
+        incapsulationExample.setAge(5);
+        incapsulationExample.setSex("male");
+
+        System.out.println(incapsulationExample);
+
+        System.out.println("########################################################");
+
+        IncapsulationExample incorrectIncapsulationExample = new IncapsulationExample();
+        incorrectIncapsulationExample.setAge(-1);
+        incorrectIncapsulationExample.setSex("unknown");
+
+        System.out.println(incorrectIncapsulationExample);
+
+        IncapsulationExample constructorExample = new IncapsulationExample("MyConstructorName");
+        System.out.println(constructorExample);
+
+        System.out.println("########################################################");
+
+        IncapsulationExample objectConstructorExample = new IncapsulationExample(constructorExample); // пример конструктора по объэкту (конструктора копирования);
+        System.out.println(objectConstructorExample);
+
+        IncapsulationExample girl_1 = new IncapsulationExample("Dina", 18, "female");
+        IncapsulationExample girl_2 = new IncapsulationExample("Dina", 18, "female");
+        IncapsulationExample girl_3 = new IncapsulationExample("Dina", 18, "female");
+
+
+    }
+}
