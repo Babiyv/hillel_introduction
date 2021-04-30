e20e5281f1c873d7b43a06f6fea45cc5348b03ed
Babiyv <babiy.vladislav@gmail.com>
2021-04-30 10:23:23 +0300

    homework Lesson 12: Array List & Linked List

--- src/com/hillel/lesson12/homework/ListClass.java
+++ src/com/hillel/lesson12/homework/ListClass.java
@@ -1,5 +1,7 @@
 package com.hillel.lesson12.homework;
 
+import com.sun.xml.internal.bind.v2.TODO;
+
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -24,16 +26,55 @@ import java.util.List;
 public class ListClass {
     public static void main(String[] args) {
 //        - Создайте Список(List);
-        List<String> newArrayList = new ArrayList<>(); // - просто для иследований по скорости выполнения дальнейшего кода
-        List<String> newLinkedList = new LinkedList<>();
+        List<Object> newArrayList = new ArrayList<>(); // - заменить Object на общий родительский класс(который нужно создать) для Class1/2/3;
+        List<Object> newLinkedList = new LinkedList<>(); // - просто для иследований по скорости выполнения дальнейшего кода
 
 //        - Создайте метод, который будет принимать и возвращать Список, и проводить с ним следующе итерации:
         firstMethod(newArrayList); // - проводил иследования по скорости выполнения. В этом раунде ArrayList проиграл 0.042 секунд (в процентном соотношении это очень большая разница);
         firstMethod(newLinkedList);
 
+//        TODO: убрать дубли кода и выводы на консоль внутри всех методов
+        secondMethod(newArrayList); // - дичайшие костыли с дублированием кода, уже даже пробовал передавать класс внутрь метода но что-то мне кажется, что сильно мудрю (да и instanceof не хотел принимать переменную передаваемого класса);
+        secondMethod(newLinkedList);
+    }
 
-
-
+    private static List secondMethod(List<Object> list) {
+        boolean booleanVariable;
+        int a = 0;
+        LocalTime localTime = LocalTime.now();
+        System.out.println("Method started at: " + localTime.toString());
+        for (int i = 0; i < list.size(); i++) {
+            booleanVariable = list.get(i) instanceof Class1;
+            if (booleanVariable){
+                a += 1;
+            }
+            if (booleanVariable && a >= 3){
+                list.remove(i);
+            }
+        }
+        a = 0;
+        for (int i = 0; i < list.size(); i++) {
+            booleanVariable = list.get(i) instanceof Class2;
+            if (booleanVariable){
+                a += 1;
+            }
+            if (booleanVariable && a >= 3){
+                list.remove(i);
+            }
+        }
+        a = 0;
+        for (int i = 0; i < list.size(); i++) {
+            booleanVariable = list.get(i) instanceof Class3;
+            if (booleanVariable){
+                a += 1;
+            }
+            if (booleanVariable && a >= 3){
+                list.remove(i);
+            }
+        }
+        System.out.println(list);
+        System.out.println("Method finished at: " + localTime.now().toString());
+        return list;
     }
 
     private static List firstMethod(List listName) {
